eb246c42a8aaad38f96d7e9ceae27cc64bbeb8d7
GraceJonn123 <gj.gjonn@gmail.com>
2016-04-13 08:32:18 +0300

    simple todos

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -2,7 +2,7 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: [
-      { text: "This is task 1" },
+      { text: "Gerrarrahia" },
       { text: "This is task 2" },
       { text: "This is task 3" }
     ]
